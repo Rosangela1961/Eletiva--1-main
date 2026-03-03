05355710a8df566396ca3b67a399793abff6b812
Rosangela1961 <gomesdossantosrosangela70@gmail.com>
2026-03-03 19:53:14 -0300

    foi feita alteração no exercicio 3

--- Lista2/exercicio3.php
+++ Lista2/exercicio3.php
@@ -3,12 +3,12 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 2</title>
+<title>Exercício 3</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1>Exercício 2</h1>
+<h1>Exercício 3</h1>
 <form method="post">
 <div class="mb-3">
               <label for="valor1" class="form-label">Informe o primeiro valor</label>
@@ -23,8 +23,8 @@
     if($_SERVER['REQUEST_METHOD'] == "POST"){
         $valor1 = $_POST['valor1'];
         $valor2 = $_POST['valor2'];
-        $subtracao = $valor2 - $valor1;
-        echo "A subtração é: $subtracao";
+        $resultado = $valor1 * $valor2;
+        echo "O resultado é: $resultado";
     }
 ?>
 
