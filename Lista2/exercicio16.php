37aea5043b44644bba2649a65408eb5a0c17c60f
Rosangela1961 <gomesdossantosrosangela70@gmail.com>
2026-03-13 21:37:05 -0300

    fiz o exercicio 16

---
+++ Lista2/exercicio16.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 16</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercicio 16</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="valor1" class="form-label">Insire um preço</label>
+              <input type="number" id="valor1" name="valor1" class="form-control">
+            </div><div class="mb-3">
+              <label for="valor2" class="form-label">Insire um percentual desconto</label>
+              <input type="number" id="valor2" name="valor2" class="form-control">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $preco = $_POST[ 'valor1'];
+    $per_desconto = $_POST['valor2'];
+    $preco_desc= $preco - ($preco * $per_desconto) / 100;
+    echo "O preço com desconto é: $preco_desc";
+
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
