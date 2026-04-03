5d7325be96cf7c5eac44aa9f1e836ddc094f1447
Rosangela1961 <gomesdossantosrosangela70@gmail.com>
2026-04-03 15:44:05 -0300

    fiz o exercicio 14

---
+++ Lista4/exercicio14.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 14</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 14</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="palavra" class="form-label">Inserir uma palavra</label>
+              <input type="text" id="palavra" name="palavra" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $palavraOriginal = mb_strtolower(trim($_POST['palavra']));
+    $palavraInvertida = strrev($palavraOriginal);
+    if ($palavraOriginal === $palavraInvertida){
+        echo "A palavra '$palavraOriginal' é um políndromo!<p>";
+
+    }else{
+        echo "A palavra '$palavraOriginal'não é um políndromo!<p>";
+        echo "Invertida ela fica: $palavraInvertida<p> ";
+    }
+}
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html> 
